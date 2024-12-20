e5d997a7455385f279bfcb29148a7f3f7e326d8f
christophernagel <christopherrrrnagel@gmail.com>
2024-12-20 15:42:03 -0800

    Add build files

---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '2f9d841d7984e3a9f530');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '6dfca5f5e9aff9e6345e');
